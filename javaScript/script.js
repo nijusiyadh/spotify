dde3c2dced0ad79367279925fb8e3f962b3790af
nijusiyadh <nijusiyadh2003@gmail.com>
2024-05-25 10:19:57 +0530

    completed

---
+++ javaScript/script.js
@@ -0,0 +1,14 @@
+// function setContainerHeight() {
+//     var container = document.querySelector('.img-container');
+//     var containerWidth = container.offsetWidth; // Get the width of the container
+//     container.style.height = containerWidth + 'px'; // Set the height equal to the width
+// }
+
+// // Call the function initially and whenever the window is resized
+// window.onload = function() {
+//     setContainerHeight();
+// };
+
+// window.onresize = function() {
+//     setContainerHeight();
+// };
